21645d6d7975771545299cdc3edb829390d33bfe
GaripovAynur <kuyan911@gmail.com>
2020-10-12 15:06:01 +0300

    Delete Проверяет встречается ли телефонный номер.py

--- python_book/Проверяет встречается ли телефонный номер.py
+++
@@ -1,9 +0,0 @@
-#SearchPhoneNumber
-"""Проверяет встречается ли телефонный номер \d{3}-\d{3}-\d{4}"""
-
-import re
-
-phoneNumRegex = re.compile(r'\d{3}-\d{3}-\d{4}')
-mo = phoneNumRegex.search('МОй НОМЕР : 324-354-3581.')
-print('Найденный телефонный номер: '  + mo.group())
-
