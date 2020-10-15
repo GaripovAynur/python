49890ca597112c69f14f99c4cdcef571fcac2ee4
GaripovAynur <kuyan911@gmail.com>
2020-10-15 16:00:31 +0300

    Update Zip.py

--- python_book/Zip.py
+++ python_book/Zip.py
@@ -13,3 +13,14 @@ spamINfo.extracе('spam.txt') #извлечь конкретный файл
 newZip = zipfile.ZipFile('new.zip', 'w') #создать новый zip
 newZip.write('spam.txt', compress_type=zipfile.ZIP_DEFLATED) # сжать
 newZip.close() #закрыть
+
+#ПРИМЕР
+#Для записи всех файлов в директории можно воспользоваться функцией os.walk:
+import zipfile
+import os
+
+z = zipfile.ZipFile('spam.zip', 'w')        # Создание нового архива
+for root, dirs, files in os.walk('folder'): # Список всех файлов и папок в директории folder
+for file in files:
+   z.write(os.path.join(root,file))         # Создание относительных путей и запись файлов в архив
+z.close()                                   # Закрытие архива
